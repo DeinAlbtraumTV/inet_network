a17125e6c3b1a66a0fccbaaa6c0567010c10cc76
Jasper Gomes <jaspi2004@gmail.com>
2024-01-14 23:27:36 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const cheerio = require('cheerio');
+const { default: axios } = require('axios');
+
+app.use(express.static('public'));
+
+app.get("/scrape", async (req, res) => {
+    let url = req.query["url"]
+
+    if (url == undefined || url == null) {
+        console.log("Invalid Request:", req.query["url"])
+        res.status(400).send("No url provided");
+        return;
+    }
+
+    let data = []
+
+    try {
+        let site = await (await axios.get(url)).data
+
+        const $ = cheerio.load(site)
+
+        $('a[href]').each((i, elem) => {
+            let obj = $(elem)
+            data.push(obj.attr("href").split("?")[0].replace("https://www.", "https://").replace("http://www.", "http://"))
+        })
+
+        res.send({
+            parent: url,
+            data: data
+        })
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
---
+++ public/code.js
@@ -0,0 +1,148 @@
+const cy = cytoscape({
+    container: document.getElementById("graph"),
+    style: [
+        {
+            "selector": "node[label]",
+            "style": {
+                "label": "data(label)",
+                "text-valign": "center",
+                "text-halign": "center",
+                "color": "white"
+            }
+        },
+      
+        {
+            "selector": "edge[label]",
+            "style": {
+                "label": "data(label)",
+                "width": 3,
+                "text-valign": "center",
+                "text-halign": "center"
+            }
+        },
+
+        {
+            "selector": ".internal",
+            "style": {
+                "line-color": "cyan",
+                "background-color": "cyan"
+            }
+        },
+      
+        {
+            "selector": ".multiline-manual",
+            "style": {
+                "text-wrap": "wrap"
+            }
+        },
+      
+        {
+            "selector": ".multiline-auto",
+            "style": {
+                "text-wrap": "wrap",
+                "text-max-width": 80
+            }
+        },
+      
+        {
+            "selector": ".autorotate",
+            "style": {
+                "edge-text-rotation": "autorotate"
+            }
+        },
+      
+        {
+            "selector": ".background",
+            "style": {
+                "text-background-opacity": 1,
+                "color": "#fff",
+                "text-background-color": "#888",
+                "text-background-shape": "roundrectangle",
+                "text-border-color": "#000",
+                "text-border-width": 1,
+                "text-border-opacity": 1
+            }
+        },
+      
+        {
+            "selector": ".outline",
+            "style": {
+                "color": "#fff",
+                "text-outline-color": "#888",
+                "text-outline-width": 3
+            }
+        }
+      ],
+    elements: [],
+})
+
+async function doScrape(_url, depth, currentDepth = 0) {
+    console.log("Running fetch for depth: ", depth, " and current depth: ", currentDepth)
+    fetch("/scrape?url=" + encodeURIComponent(_url), {cache: "no-store"}).then(async (res) => {
+        let resData = await res.json()
+        let graphData = {
+            nodes: [],
+            edges: []
+        }
+
+        graphData.nodes.push({
+            data: {
+                id: resData.parent,
+                label: resData.parent
+            }
+        })
+
+        resData.data.forEach(element => {
+            let url = new URL(element, resData.parent)
+
+            let isInternal = url.hostname == new URL(resData.parent).hostname
+
+            graphData.nodes.push({
+                data: {
+                    id: url.href,
+                    label: url.href,
+                    internal: isInternal
+                },
+                classes: [
+                    (isInternal ? "internal" : "")
+                ]
+            })
+
+            graphData.edges.push({
+                data: {
+                    id: resData.parent + ":" + url.href,
+                    source: resData.parent,
+                    target: url.href,
+                    internal: isInternal
+                },
+                classes: [
+                    (isInternal ? "internal" : "")
+                ]
+            })
+
+            if (currentDepth + 1 < depth) {
+                doScrape(url.href, depth, currentDepth + 1)
+            }
+        });
+
+        cy.add(graphData)
+    })
+}
+
+function applyLayout() {
+    cy.center()
+    cy.layout({name:"fcose",nodeDimensionsIncludeLabels:true,avoidOverlaps:true}).run()
+}
+
+document.getElementById("layout-button").onclick = event => {
+    applyLayout()
+}
+
+document.getElementById("add-node-form").onsubmit = event => {
+    event.preventDefault();
+    let formData = new FormData(event.target)
+    let formProps = Object.fromEntries(formData)
+    
+    doScrape(formProps.url, formProps.depth)
+}
+
